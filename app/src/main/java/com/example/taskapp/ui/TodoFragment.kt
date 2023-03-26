045ebce00affc5e96360ac53da06e9f6bc57bee6
MarcosAndre28 <marcosandre1728@gmail.com>
2023-03-26 20:54:45 -0300

    Recuperando tarefas do Firebase Realtime Database

--- app/src/main/java/com/example/taskapp/ui/TodoFragment.kt
+++ app/src/main/java/com/example/taskapp/ui/TodoFragment.kt
@@ -14,7 +14,14 @@ import com.example.taskapp.data.model.Status
 import com.example.taskapp.data.model.Task
 import com.example.taskapp.databinding.FragmentTodoBinding
 import com.example.taskapp.ui.adapter.TaskAdapter
-import com.example.taskapp.ui.adapter.TaskTopAdapter
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 
 class TodoFragment : Fragment() {
@@ -23,7 +30,10 @@ class TodoFragment : Fragment() {
     private val binding get() = _binding!!
 
     private lateinit var taskAdapter: TaskAdapter
-    private lateinit var taskTopAdapter: TaskTopAdapter
+
+    private lateinit var reference: DatabaseReference
+    private lateinit var auth: FirebaseAuth
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,6 +45,9 @@ class TodoFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initListener()
+
+        reference = Firebase.database.reference
+        auth = Firebase.auth
 
         initRecyclerView()
         getTask()
@@ -48,20 +61,15 @@ class TodoFragment : Fragment() {
 
     private fun initRecyclerView(){
 
-        taskTopAdapter = TaskTopAdapter { task, option ->
-            optionSelected(task,option)
-        }
-
         taskAdapter = TaskAdapter(requireContext()){task, option ->
             optionSelected(task,option)
         }
 
-        val concatAdapter = ConcatAdapter(taskTopAdapter,taskAdapter)
 
         with(binding.rvTasks){
             layoutManager = LinearLayoutManager(requireContext())
             setHasFixedSize(true)
-            adapter = concatAdapter
+            adapter = taskAdapter
 
         }
     }
@@ -88,24 +96,22 @@ class TodoFragment : Fragment() {
     }
 
     private fun getTask() {
+        reference.child("tasks")
+            .child(auth.currentUser?.uid ?: "")
+            .addValueEventListener(object : ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val taskList = mutableListOf<Task>()
+                   for (ds in snapshot.children){
+                       val task = ds.getValue(Task::class.java) as Task
+                       taskList.add(task)
+                   }
+                    taskAdapter.submitList(taskList)
+                }
 
-        val teskTopList = listOf(
-            Task("0","Top list",Status.TODO),
-        )
-
-     val teskList = listOf(
-         Task("0","Criar nova tela do app",Status.TODO),
-         Task("1","Validar informações na tela de login",Status.TODO),
-         Task("2","Adicionar nova funcionalidade no app",Status.TODO),
-         Task("3","Salvar token no localmente",Status.TODO),
-         Task("4","Criar funcionalidade de logout no app",Status.TODO)
-     )
-
-
-
-        taskTopAdapter.submitList(teskTopList)
-
-        taskAdapter.submitList(teskList)
+                override fun onCancelled(error: DatabaseError) {
+                    Toast.makeText(requireContext(), R.string.error_generic, Toast.LENGTH_SHORT).show()
+                }
+            })
     }
 
 
